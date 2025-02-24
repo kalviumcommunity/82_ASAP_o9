7d8883d33d2eb515992517b99aa2f26ddf3d0a10
Aizen09 <scilentgamer99@gmail.com>
2025-02-24 15:17:02 +0530

    ASAP_4 created simple server for deploying in render

--- Server.js
+++ Server.js
@@ -12,14 +12,14 @@ app.get('/', (req,res) => {
 })
 
 app.post('/ping' , (req,res) => {
-
+   
     try{
-        const {name,email,phNumner} = req.body;
-        if(!name || !email){
+        const {name,email,phNumber} = req.body;
+        if(!name || !email ||!phNumber){
             return res.status(400).json({message : "All input fields are required"});
         }
     
-        const len = phNumner.length;
+        const len = phNumber.length;
         if(len<10 || len>10){
             return res.status(400).json({message : "Invalid phNumber"});
         }
@@ -33,6 +33,11 @@ app.post('/ping' , (req,res) => {
     }
 } )
 
-app.listen(port, (req,res) => {
-    console.log(chalk.blue(`Server is running Successfully at http://localhost:${port}`))
+app.listen(port, () => {
+    try {
+        console.log(chalk.blue(`Server is running Successfully at http://localhost:${port}`))
+    } catch (error) {
+         console.log(error)
+    }
+    
 })
