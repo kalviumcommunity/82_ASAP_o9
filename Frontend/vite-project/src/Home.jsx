7f5e32ddbeb19a93f966c54935ead976c6cbbbe6
Aizen09 <scilentgamer99@gmail.com>
2025-03-06 13:43:56 +0530

    Created Home page for frontend

---
+++ Frontend/vite-project/src/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+    return(
+        <div style={{textAlign : "center",marginTop : "50px"}}>
+            <h1>Mohit's ASAP Home Page</h1>
+            <p>Don't tap anything</p>
+            <p>There  is no Animation for any feature yet</p>
+            <p>The Backend is not Created yet</p>
+            <p>See you! soon Sayonara</p>
+
+        </div>
+    );
+}
+
+export default Home;
